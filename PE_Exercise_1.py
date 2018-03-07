b0cce82b308dc152c52b1de05a4764c2b0e04062
tshiu <tracey.shiu@gmail.com>
2018-03-07 15:52:32 -0800

    Add files via upload

---
+++ PE_Exercise_1.py
@@ -0,0 +1,36 @@
+
+Write a script that calculates, for a given application, how many total teachers are shared across all of its districts.
+
+#Would like to get input as to what record we are looking to query
+var {record_type} = prompt("What record type would you like to query?")
+
+print record_type #print the prompt so that we can verify that the record type is valid (e.g. if they wrote in teacher ID instead of teahcer)
+
+#will need to retrieve the tokens for all of the districts as this is based on each district-app connection
+GET tokens at https://clever.com/oauth/tokens?owner_type=district
+#setting count at 0 to start
+count = 0
+#setting response as nil now so that it can be called within the loop-like function
+response = nil 
+#setting path as the endpoint, looking to put record_type from prompt into the endpoint
+path = /v2.0/{record_type}
+
+#for each district-app connection
+For each token
+
+do {
+	#using token to ping the API to retrieve the JSON response
+	response = GET https://api.clever.com+path
+	#counting the length of the json response objects, assuming that if there are 4 teachers shared, there will be four responses so just counting those data blobs
+	count = count + response.data.length
+	#looking to maybe print here to see what might be happening in case there is a mistake
+	print count #would like to see how this final number is tallied to avoid looping through the data indefinitely
+	#taking the pagination link and appending that to the end of the URI in case there are more responses
+	path = response.links[1].uri 
+#hwould like to continue to count until there is no pagination link at the end
+} while (path is present);
+
+#print the final tally
+console.log(count)
+
+
---
+++ PE_Exercise_2.py
@@ -0,0 +1,38 @@
+Write a script that calculates, for a given district-app connection, how many shared students are associated with multiple schools.
+
+var {district_id} = prompt("What is the district_id?");
+
+#print the prompt so that we can verify that the ID is valid 
+print district_id 
+
+#I want to retrieve the district specific token at this endpoint
+GET token at https://clever.com/oauth/tokens?owner_type=district&district={district_id}'
+
+#starting the count at 0 and defining count
+count = 0
+
+#defining the response; setting a variable?
+response = nil
+
+#this is the path/endpoint that will be attached onto the URL to access the students endpoint; this is the initial endpoint that is used
+path = /v2.0/students
+
+#setting a loop-like function so that we can repeat the steps below until 
+do {
+	#looking to retrieve the response from the students endpoint which will return a JSON response
+	response = GET https://api.clever.com+path
+
+	#paging through each student in the response body
+	For each student in response.data
+
+		If student.schools.length > 1, 
+			#looking if the student is associated with more than 1 school, I am looking to add 1 to the count
+			then count++ 
+
+	path = response.links[1].uri #will need to modify this path as the URI is respnonse.link[2].uri after the initial response
+	#perhaps print path after the retrieval to confirm the uri is correct
+#if the response has the path present, then we will want to take the URI, attached this to the endpoint and make another call, once it is not present, we want to stop this loop
+} while (path is present);
+
+#should print the final count
+console.log(count) 
