54e783c6045065827092f7ae3b038eacd934d433
tshiu <tracey.shiu@gmail.com>
2018-03-12 14:22:28 -0700

    Updated PE_Exercise_2.py
    Should be up to milestone 5

--- PE_Exercise_2.py
+++ PE_Exercise_2.py
@@ -1,11 +1,11 @@
 #Write a script that calculates, for a given district-app connection, 
 #how many shared students are associated with multiple schools.
 
-print "What is the district_id?"
-district_id = raw_input()
+from sys import argv
 
-#print the prompt so that we can 
-#verify that the ID is valid; perhaps we can write in a check here against another call 
+script, district_id = argv
+
+#print the prompt so that we can verify that the ID is valid 
 print district_id 
 
 #I want to retrieve the district specific token at this endpoint
@@ -17,8 +17,8 @@ count = 0
 #defining the response; setting a variable?
 response = nil
 
-#this is the path/endpoint that will be attached onto the URL to access the students endpoint; 
-#this is the initial endpoint that is used
+#this is the path/endpoint that will be attached onto the URL to access 
+#the students endpoint; this is the initial endpoint that is used
 path = /v2.0/students
 
 #setting a loop-like function so that we can repeat the steps below until 
@@ -29,22 +29,14 @@ do {
 	#paging through each student in the response body
 	For each student in response.data
 
-		If student.schools.length > 1, 
+		If student.schools.len > 1, 
 			#looking if the student is associated with more than 1 school, I am looking to add 1 to the count
 			then count++ 
 
-			#check to see if count is a real number, or if we have an actual response
-			print count 
-	#will need to modify this path as the URI is respnonse.link[2].uri after the initial response		
-	path = response.links[1].uri 
-	
-	#print path after the retrieval to confirm the uri is correct
-	print path 
-
-#if the response has the path present, then we will want to take the URI, 
-#attached this to the endpoint and make another call, once it is not present, we want to stop this loop
+	path = response.links[1].uri #will need to modify this path as the URI is respnonse.link[2].uri after the initial response
+	#perhaps print path after the retrieval to confirm the uri is correct
+#if the response has the path present, then we will want to take the URI, attached this to the endpoint and make another call, once it is not present, we want to stop this loop
 } while (path is present);
 
 #should print the final count
-
-print count
+print count 
