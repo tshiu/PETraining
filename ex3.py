cc6c90256f01b2f3a2cf80776b3af352698c9d5d
tshiu <tracey.shiu@gmail.com>
2018-03-14 21:46:35 -0700

    ex3.py

--- ex3.py
+++ ex3.py
@@ -3,16 +3,16 @@ print "I will now count my chickens:"
 
 #Will print what is in quotes and then the actual calculations
 print "Hens", 25.0 + 30.0 / 6.0 
-print "Roosters", 100 - 25 * 3 % 4 
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0 
 
 print "Now I will count the eggs:"
 
 #will calculate according to pemdas :)
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0 
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
-print 3 + 2 < 5 - 7 
+print 3.0 + 2.0 < 5.0 - 7.0 
 
 print "What is 3 + 2?", 3 + 2
 print "What is 5 - 7?", 5 - 7 
@@ -25,5 +25,18 @@ print "How about some more."
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2 
 print "Is it less or equal?", 5 <= -2
+
+#SD
+# 1. Above
+# 2. I typed 'python' and did a few calculations. It works!
+# 3. I wasn't sure what this prompt meant— did it want a simple calucation? I just picked
+#    a random calculation
+
+#    print "What is 55 * 55?", 55 * 55
+# 4. Floating point numbers are numbers with increased precision as they have numbers 
+#    after the decimal place; non-integers
+# 5. Rewritten above. I replaced the calculated numbers. 
+
+
 
 
