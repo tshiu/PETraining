d73c9257ecdfc176dae8050e858ffa7f8759e20f
tshiu <tracey.shiu@gmail.com>
2018-03-14 21:54:50 -0700

    update 14.py

--- ex14.py
+++ ex14.py
@@ -3,7 +3,7 @@ from sys import argv
 script, user_name, time_played = argv 
 #setting prompt as a variable, so we do not have to
 #retype the carrot each time
-prompt = '> '
+prompt = 'Answer here'
 
 #will insert display the input from the arg
 print "Hi %s, I'm the %s script. I hear its your %s time playing" % (user_name, script, time_played)
@@ -23,3 +23,11 @@ Alright, so you said %r about liking me.
 You live in %r. Not sure where that is.
 And you have a %r computer. Nice.
 """ % (likes, lives, computer)
+
+#SD
+#1. I downloaded a copy but my responses did not work. It is in my drive but I suspect it might be out of date?
+# then I just played it online here: http://textadventures.co.uk/games/play/5zyoqrsugeopel3ffhz_vq 
+# Seems to be game where you plug in text based on accempted inputs
+#2. Above! Changes to "answer here"
+#3. Added time_played as an arg
+#4. Yes! Printing multiple lines while calling variables.
