75c360f3a3bf9584ba07874eabd1321aafa6c379
tshiu <tracey.shiu@gmail.com>
2018-03-26 20:44:36 -0700

    Update PE_Exercise_1.py

--- PE_Exercise_1.py
+++ PE_Exercise_1.py
@@ -1,7 +1,8 @@
-#Prompt: Write a script that calculates, for a given application, how many total teachers are shared across all of its districts.
+#Prompt: Write a script that calculates, for a given application, how many total 
+#teachers are shared across all of its districts.
 
 #Would like to get input as to what record we are looking to query, will probably change this to argv module
-record_type = raw_input("Which record type to query? (district_admins, teachers, students, sections, school_admins)")
+record_type = raw_input("Which record type to query?")
 
 #printing this input as a debugging so that we can see if the user typed in the right format
 print record_type 
@@ -14,21 +15,21 @@ Is this correct? Press enter to proceed or CTRL-C to escape.
 
 raw_input()
 
-	#setting count at 0 to start
-	count = 0
+#setting count at 0 to start
+count = 0
 
-	#setting response as nil now so that it can be called within the loop-like function
-	response = nil 
+#setting response as nil now so that it can be called within the loop-like function
+response = nil 
 
-	#setting path as the endpoint to put record_type from prompt into the endpoint
-	path = /v2.0/{record_type}
+#setting path as the endpoint to put record_type from prompt into the endpoint
+path = /v2.0/{record_type}
 
-	#will set client ID
-	client_ID = [Client_ID_for_district]
-	#will set client secret 
-	client_secret = [Client_Secret_for_district]
+#will set client ID
+client_ID = [Client_ID_for_district]
+#will set client secret 
+client_secret = [Client_Secret_for_district]
 
-def get_tokens(district):
+def get_tokens():
 
 	#will need to retrieve the tokens for all of the districts as 
 	#this is based on each district-app connection
@@ -40,15 +41,25 @@ def get_tokens(district):
     
     return list_of_tokens
 
-get_tokens
+get_tokens()
 
-for token in list_of_tokens:
+def page_through():
+#checking to see if the paging link is present
+	while response.links[1].uri is present: 
 
-total_users 
+	#replacing path with the paging link that is prsent 	
+		path = response.links[1].uri 
 
-def total_users(record_type,district):
-
+		directing user to go back to adding to the total user count with the next path if link
+		#is present
+		total_users()
+			
 	
+	move on to the next token in the list 
+
+
+def total_users(record_type):
+
 	#using token to ping the API to retrieve the JSON response
 	response = GET https://api.clever.com+path
 	
@@ -62,19 +73,8 @@ def total_users(record_type,district):
 
 	page_through()
 
-#will need to 
-	def page_through():
-	#checking to see if the paging link is present
-		if response.links[1].uri is present 
-
-		#replacing path with the paging link that is prsent 	
-			path = response.links[1].uri 
-
-		#directing user to go back to adding to the total user count with the next path if link
-		#is present
-			total_users()
-			
-		else:
-			move on to the next token in the list 
+for token in list_of_tokens:
+#the record type is set from before
+total_users(record_type)
 
 
