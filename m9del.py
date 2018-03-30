6b9e78ced30b703c7060bcd4403aa084ece88720
tshiu <tracey.shiu@gmail.com>
2018-03-30 15:46:56 -0700

    updated m9del.pu

--- m9del.py
+++ m9del.py
@@ -26,13 +26,13 @@ while guess != number:
 		
 	new_guess = int(raw_input("Lets try again. What's your guess? \n"))
 
-	if new_guess == guess:
+	while new_guess == guess:
 		print "That was the exact same guess. Try again."
-		guess = int(raw_input("Lets try once more. Pick a different number \n"))
+		new_guess = int(raw_input("Lets try once more. Pick a different number \n"))
 		#Why does setting new_guess to guess not work?
 		#Answer: I had a type that was setting input to new_guess instead of guess
-		new_guess = guess
-		guess_taken = guess_taken + 1
+		#new_guess = guess
+		#guess_taken = guess_taken + 1
 		#These were included when I was troubleshooting what wasn't working
 		#print "This is the if block"
 		#print "This is the new_guess: %d " % new_guess
@@ -40,19 +40,17 @@ while guess != number:
 		#print "This is the guess: %d " % guess
 
 
-	elif new_guess != guess:
-		guess = new_guess
-		guess_taken = guess_taken + 1
-		#print "this is the elif block"
-		#print "The is the new_guess: %d " % new_guess
-		#print "This is the number: %d " % number
-		#print "This is the guess: %d " % guess
+	
+	guess = new_guess
+	guess_taken = guess_taken + 1
+	#print "this is the elif block"
+	#print "The is the new_guess: %d " % new_guess
+	#print "This is the number: %d " % number
+	#print "This is the guess: %d " % guess
 
 
-
-else:
-	print "Yes, the number was %d! You win!" % guess
-	print "You took %d guesses to get the answer. Not bad!" % guess_taken
+print "Yes, the number was %d! You win!" % guess
+print "You took %d guesses to get the answer. Not bad!" % guess_taken
 		
 		
 		
